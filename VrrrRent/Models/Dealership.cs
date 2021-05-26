d35b8fc3656de16b7b3e8a766baadcabec7239da
GarlicDoggo <81585015+GarlicDoggo@users.noreply.github.com>
2021-05-26 22:44:50 +0300

    autorizari

--- VrrrRent/Models/Dealership.cs
+++ VrrrRent/Models/Dealership.cs
@@ -9,6 +9,7 @@ namespace VrrrRent.Models
     {
         public int ID { get; set; }
         public string City { get; set; }
+        public string Adress { get; set; }
         public ICollection<Inventory> Inventory { get; set; }
 
     }
