a371f2404ec54fef9949c4a8da1d6d7be06e8d40
Mihaitzaa <oprea.mihai.x7n@student.ucv.ro>
2021-05-27 23:22:46 +0300

    some stuff

--- VrrrRent/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VrrrRent/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -57,6 +57,22 @@ namespace VrrrRent.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "Username: ")]
+            public string Username { get; set; }
+
+            [Required]
+            [Display(Name = "First Name: ")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name: ")]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "Phone Number: ")]
+            public string PhoneNumber { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
